a2a0c09a33c2d9d452158d61c25b253edc61845c
morzh <morzh80@mail.ru>
2024-06-30 22:29:48 +0300

    refactoring

--- datasets/youtube_database/experiments/yt-dlp_test_002.py
+++
@@ -1,9 +0,0 @@
-from utils.youtube.fetch_information import fetch_youtube_channel_information
-
-
-# youtube_channel_url = 'https://www.youtube.com/@eylemabaci'
-youtube_channel_url = 'https://www.youtube.com/@drakestephens3447'
-
-current_info = fetch_youtube_channel_information(youtube_channel_url, print_info=True)
-
-print(len(current_info['tags']))
