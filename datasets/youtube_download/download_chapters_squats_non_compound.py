4ccddb6d063e1207b49bdd808af34c6c66ce95a5
morzh <morzh80@mail.ru>
2024-08-20 23:18:36 +0300

    download chapters with squats #06

--- datasets/youtube_download/download_chapters_squats_non_compound.py
+++ datasets/youtube_download/download_chapters_squats_non_compound.py
@@ -5,27 +5,28 @@ from utils.youtube_links_database.database_promts import chapters_data_via_promt
 from utils.youtube_links_database.download_video import download_youtube_video_chapter
 
 
-def download_chapters_squats_non_compound(database_filepath: str, promts_filepath: str, output_folder: str, **kwargs) -> None:
+def download_youtube_chapters_videos(database_filepath: str, promts_filepath: str, output_folder: str, **kwargs) -> None:
     """
     Description:
+        This functions downloads chapters from YouTube.
 
-    :param database_filepath:
-    :param promts_filepath:
+    :param database_filepath: SQLite3 links database filepath
+    :param promts_filepath: File path to .json file with include and exclude tokens for database requests
     :param output_folder: folder for downloaded videos
 
-    :key each_include_promt_to_separate_folder:
-    :key video_chapter_offset_seconds:
-    :key video_format:
-    :key video_quality:
-    :key video_format:
-    :key print_links:
-    :key print_chapters_data:
-    :key print_chapters_name:
-    :key use_proxy:
+    :key each_include_promt_to_separate_folder: use include promt tokens to sort downloaded videos by subfolders
+    :key video_chapter_offset_seconds: offsets YouTube chapters start and end time in seconds ([seconds_start - offset, seconds_end + offset])
+    :key video_format: video format, e.g. 'mp4'
+    :key video_quality: video quality, e.g. 720
+    :key print_links: if True prints links to YouTube chapters
+    :key print_chapters_data: if it is True prints chapters data
+    :key print_chapters_name: if True prints chapters names
+    :key use_proxy: proxy for connection to 'youtube.com'
     """
 
     key_per_include_promt = kwargs['each_include_promt_to_separate_folder']
     chapters_promts = chapters_data_via_promts(database_filepath, promts_filepath)
+    video_format = kwargs.get('video_format','mp4')
     total_chapters_number = sum(len(l) for l in chapters_promts.values())
     print('Overall number of chapters is', total_chapters_number)
 
@@ -41,8 +42,11 @@ def download_chapters_squats_non_compound(database_filepath: str, promts_filepat
             current_video_id = chapter[5]
             current_time_start = max(0, int(chapter[2]) - offset)
             current_time_end = int(chapter[3]) + offset
-            current_output_file_basename = f'{current_video_id}__{current_time_start}-{current_time_end}__'
-            current_output_filepath = os.path.join(current_output_folder, current_output_file_basename)
+            current_output_filename = f'{current_video_id}__{current_time_start}-{current_time_end}__.{video_format}'
+            current_output_filepath = os.path.join(current_output_folder, current_output_filename)
+
+            if os.path.exists(current_output_filepath) and os.stat(current_output_filepath).st_size > 1024:
+                continue
 
             download_youtube_video_chapter(current_video_id,
                                            current_output_filepath,
@@ -72,4 +76,4 @@ if __name__ == '__main__':
 
     output_videos_folder = '/home/anton/work/fitMate/datasets/squats_non_compound'
 
-    download_chapters_squats_non_compound(database_file_path, promts_file_path, output_videos_folder, **download_parameters)
+    download_youtube_chapters_videos(database_file_path, promts_file_path, output_videos_folder, **download_parameters)
--- utils/youtube_links_database/download_video.py
+++ utils/youtube_links_database/download_video.py
@@ -6,23 +6,24 @@ from retry import retry
 
 @retry(yt_dlp.utils.DownloadError, delay=2, backoff=2, max_delay=4, tries=5)
 def download_youtube_video_chapter(video_id: str,
-                                   output_file_path_basename: str,
+                                   output_filepath: str,
                                    video_segment: tuple[int, int] | None,
                                    **kwargs) -> None:
     """
     Description
         Download YouTube video chapter.
 
-    :param video_id: YouTube video ID
-    :param output_file_path_basename: output video file path base name. Video file extension will be added automatically.
-    :param video_segment: video segment [time_start, time_end] in seconds
-    :key video_chapter_offset_seconds: add offset to video segment. New segment will be [time_start - offset, time_end + offset]
+    :param video_id: YouTube video ID.
+    :param output_filepath: output filepath for downloaded video.
+    :param video_segment: video segment [time_start, time_end] in seconds.
+
+    :key video_format: add offset to video segment. New segment will be [time_start - offset, time_end + offset]
+    :key video_quality: video quality, e.g. 360, 720 or 1080
     """
     video_format = kwargs.get('video_format','mp4')
     video_quality = kwargs.get('video_quality', 720)
 
     video_temporal_filepath = f'__tmp__.{video_format}'
-    video_output_filepath = f'{output_file_path_basename}.{video_format}'
 
     if os.path.exists(video_temporal_filepath):
         os.remove(video_temporal_filepath)
@@ -47,10 +48,5 @@ def download_youtube_video_chapter(video_id: str,
     pts = 'PTS-STARTPTS'
     input_stream = ffmpeg.input(video_temporal_filepath)
     video_cut = input_stream.trim(start=video_segment[0], end=video_segment[1]).setpts(pts)
-    output_video = ffmpeg.output(video_cut, video_output_filepath, format='mp4')
+    output_video = ffmpeg.output(video_cut, output_filepath, format='mp4')
     output_video.run()
-
-
-
-
-
