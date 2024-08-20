75b11b78b2842ff27f28294d1734d50adf87a111
morzh <morzh80@mail.ru>
2024-08-21 00:00:25 +0300

    download chapters with squats #09

---
+++ datasets/youtube_build_links_database/links_from_promts_squats_non_compound.py
@@ -0,0 +1,21 @@
+import os
+from utils.youtube_links_database.database_promts import links_from_database_promts
+
+
+if __name__ == '__main__':
+    database_folder = '/home/anton/work/fitMate/datasets'
+    promts_folder = '/home/anton/work/fitMate/datasets/exercises_filter_promts'
+    database_filename = 'youtube_rep_fit_database.db'
+    promts_filename = 'squats_non_compound.json'
+
+    database_file_path = os.path.join(database_folder, database_filename)
+    promts_file_path = os.path.join(promts_folder, promts_filename)
+
+    parameters = {
+        'print_links': False,
+        'verbose': True,
+    }
+
+    chapters_links = links_from_database_promts(database_file_path, promts_file_path, **parameters)
+
+    # pprint.pprint(chapters_links)
--- utils/youtube_links_database/database_promts.py
+++ utils/youtube_links_database/database_promts.py
@@ -5,6 +5,19 @@ import json
 import numpy as np
 
 
+class Color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARK_CYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+
+
 def filter_chapters(database_filepath: str, like_entries: list[str], not_like_entries: list[str]) -> list[tuple]:
     """
     Description:
@@ -105,3 +118,41 @@ def chapters_data_via_promts(database_filepath: str, promts_filepath: str) -> di
         chapters_data[exercise] = current_chapters
 
     return chapters_data
+
+
+def links_from_database_promts(database_filepath, promts_filepath, **kwargs) -> dict[str, list]:
+    """
+    Description:
+
+    :param database_filepath:
+    :param promts_filepath:
+
+    :key print_links: print chapter links
+    :key verbose:
+
+    :return: dictionary, where each key is a squat type and value is list of links to chapters
+    """
+    verbose = kwargs.get('verbose', True)
+    print_links = kwargs.get('print_links', False)
+
+    chapters_promts = chapters_data_via_promts(database_filepath, promts_filepath)
+    links = {}
+    total_chapter_number = 0
+
+    for chapter_folder, chapters_data in chapters_promts.items():
+        print(Color.BOLD + Color.YELLOW + chapter_folder + Color.END)
+        current_chapters_links = convert_chapters_data_to_links(chapters_data)
+        links[chapter_folder] = current_chapters_links
+        if print_links:
+            pprint.pprint(current_chapters_links, indent=8, width=150)
+
+        if verbose:
+            current_number_chapters = len(chapters_data)
+            total_chapter_number += current_number_chapters
+            print(f'\t Number of chapters is {current_number_chapters}')
+
+    if verbose:
+        print('-' * 40)
+        print(Color.BOLD + Color.GREEN +  f'Total number of chapters is {total_chapter_number}' + Color.END)
+
+    return links
