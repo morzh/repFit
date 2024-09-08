2f8e3f13be57d5b164ad8710b65046d08b6bf8e2
morzh <morzh80@mail.ru>
2024-09-08 16:48:13 +0300

    download videos with squats #06

--- utils/youtube_links_database/videos_download.py
+++ utils/youtube_links_database/videos_download.py
@@ -6,6 +6,38 @@ from retry import retry
 from utils.youtube_links_database.database_promts import videos_data_from_database_promts
 
 
+@retry(yt_dlp.utils.DownloadError, delay=2, backoff=2, max_delay=4, tries=5)
+def download_single_video_from_youtube(video_id: str, output_filepath: str, use_proxy: bool, **kwargs) -> None:
+    """
+    Description
+        Download YouTube video chapter.
+
+    :param video_id: YouTube video ID.
+    :param output_filepath: output filepath for downloaded video.
+    :param use_proxy: use proxy socks5://127.0.0.1 to mitigate YouTube speed restriction in Russia
+
+    :key video_format: add offset to video segment. New segment will be [time_start - offset, time_end + offset]
+    :key video_quality: video quality, e.g. 360, 720 or 1080
+    """
+    video_format = kwargs.get('video_format', 'mp4')
+    video_quality = kwargs.get('video_quality', 720)
+
+    ydl_options = {
+        'verbose': True,
+        'format': f'bestvideo[height={video_quality}]',
+        'force_keyframes_at_cuts': True,
+        'outtmpl': output_filepath,
+        'prefer_ffmpeg': True,
+    }
+    if kwargs.get('use_proxy', False):
+        ydl_options['proxy'] = "socks5://127.0.0.1"
+
+    youtube_video_url = f'https://www.youtube.com/watch?v={video_id}'
+
+    with yt_dlp.YoutubeDL(ydl_options) as ydl:
+        ydl.download(youtube_video_url)
+
+
 def download_youtube_videos(database_filepath, promts_filepath, output_folder, config) -> None:
     """
     Description:
@@ -21,6 +53,7 @@ def download_youtube_videos(database_filepath, promts_filepath, output_folder, c
     videos_data = videos_data_from_database_promts(database_filepath, promts_filepath)
     video_options = config['video_options']
     debug_options = config['debug_videos_options']
+    use_proxy = config['connection']['use_proxy']
     print('Overall number of chapters is', len(videos_data))
 
     for video_data in videos_data:
@@ -30,7 +63,7 @@ def download_youtube_videos(database_filepath, promts_filepath, output_folder, c
         current_output_filepath = os.path.join(output_folder, current_output_filename)
 
         try:
-            download_single_video_from_youtube(current_video_id, current_output_filepath, **kwargs)
+            download_single_video_from_youtube(current_video_id, current_output_filepath, use_proxy, **video_options)
             if debug_options['print_chapters_links']:
                 print(f'{current_video_link} -> {current_output_filename}')
 
