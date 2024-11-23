b6efff33760bdd26aad78b91bad8e45a739cf74f
morzh <morzh80@mail.ru>
2024-11-23 23:48:44 +0300

    bounding box #72

--- core/filters/single_person/core/filters/bridge_gaps_filter_addon.py
+++ core/filters/single_person/core/filters/bridge_gaps_filter_addon.py
@@ -4,14 +4,21 @@ from core.filters.single_person.core.multiple_persons_tracks import MultiplePers
 
 class BridgeGapsFilterAddon(MultiPersonsFilterAddonBase):
     """
+    Description:
+        Filter for bridging gaps between frames segments.
+        If gap between neighbouring segments is less than the given threshold  value, this segments will be fused into one.
 
+    :ivar gap_threshold: gap threshold
     """
     def __init__(self, gap_threshold=5.0):
-        self.threshold = gap_threshold
+        self.gap_threshold = gap_threshold
 
     def process(self, tracks: MultiplePersonsTracks) -> None:
         """
+        Description:
+            Fill gap between neighbouring segments within given threshold.
 
+        :param tracks: person's track
         """
         persons_number = len(tracks.persons)
         video_fps = tracks.video_properties.fps
